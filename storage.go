7747bc29065ec1a918d1b865a9fd3de19b1c19f6
Aditya <vaghasiyaaditya123@gmail.com>
2025-07-16 23:50:50 +0530

    with Del Func

--- storage.go
+++ storage.go
@@ -13,8 +13,11 @@ import (
 
 type PathTransFormFunc func(string) PathKey
 
-var DefaultPathTransportFunc = func(key string) string {
-	return key
+var DefaultPathTransportFunc = func(key string) PathKey {
+	return PathKey{
+		Pathname: key,
+		Filename: key,
+	}
 }
 
 func CASPathTransformFunc(key string) PathKey {
@@ -45,16 +48,40 @@ type StoreOpts struct {
 }
 
 type Store struct {
+	Root string
 	StoreOpts
 }
 
+const DefaultRootDir = "root"
+
 func NewStore(opts StoreOpts) *Store {
+	if opts.PathTransFormFunc == nil {
+		opts.PathTransFormFunc = DefaultPathTransportFunc
+	}
 	return &Store{
+		Root:      DefaultRootDir,
 		StoreOpts: opts,
 	}
 }
 
-func (s *Store) Delete() {}
+func (s *Store) Delete(key string) error {
+	fmt.Println("key is: ", key)
+	pathkey := s.PathTransFormFunc(key)
+	// if len(pathkey.Fullpath()) == 0 {
+	// 	return nil
+	// }
+	fmt.Println("Full path:", pathkey.Fullpath())
+	//fullStr := fmt.Sprint("%s/%s", s.Root, pathkey.Fullpath())
+	SplitStr := strings.Split(pathkey.Fullpath(), "/")
+	err := os.RemoveAll(SplitStr[0])
+	if err != nil {
+		return err
+	}
+	defer func() {
+		fmt.Printf("deleted [%s] from the disk", pathkey.Fullpath())
+	}()
+	return nil
+}
 
 func (s *Store) Read(key string) (io.Reader, error) {
 	f, err := s.readStream(key)
@@ -69,37 +96,38 @@ func (s *Store) Read(key string) (io.Reader, error) {
 
 func (s *Store) readStream(key string) (io.ReadCloser, error) {
 	pathkey := s.PathTransFormFunc(key)
+	//fullPathWithStr := fmt.Sprintf("%s/%s", s.Root, pathkey.Fullpath())
 	return os.Open(pathkey.Fullpath())
 
 }
 
 func (p PathKey) Fullpath() string {
-	return fmt.Sprintf("%s/%s", p.Pathname, p.Filename)
+	return fmt.Sprintf("%s/%s/%s", DefaultRootDir, p.Pathname, p.Filename)
 }
 
 func (s *Store) WriteStream(key string, r io.Reader) error {
 	pathname := s.PathTransFormFunc(key)
 	fmt.Printf("path is: %v", pathname)
 	buf := new(bytes.Buffer)
-	err := os.MkdirAll(pathname.Pathname, os.ModePerm)
+	err := os.MkdirAll(s.Root+"/"+pathname.Pathname, os.ModePerm)
 	if err != nil {
 		return err
 	}
 	io.Copy(buf, r)
 
-	fullpath := pathname.Fullpath()
+	//fullpath := pathname.Fullpath()
 	//filename := "somefilename"
 
-	f, err := os.Create(fullpath)
+	f, err := os.Create(pathname.Fullpath())
 	if err != nil {
 		return err
 	}
-
+	defer f.Close()
 	n, err := io.Copy(f, buf)
 	if err != nil {
 		return err
 	}
-	log.Printf("written %d bytes to the disk %s", n, fullpath)
+	log.Printf("written %d bytes to the disk %s", n, pathname.Fullpath())
 
 	return nil
 }
--- storage_test.go
+++ storage_test.go
@@ -7,19 +7,19 @@ import (
 	"testing"
 )
 
-func TestHash(t *testing.T) {
-	key := "sampleKey"
-	Pathname := CASPathTransformFunc(key)
-	expectedOriginalKey := "a421c5f473312f80f7984da72bc9c05a7b48cf37"
-	ExpectedPathname := "a421c5f473/312f80f798/4da72bc9c0/5a7b48cf37"
-	if Pathname.Pathname != ExpectedPathname {
-		fmt.Printf("have %s want %s ", Pathname.Pathname, ExpectedPathname)
-	}
-	if Pathname.Filename != expectedOriginalKey {
-		fmt.Printf("have %s want %s ", Pathname.Filename, expectedOriginalKey)
-	}
+// func TestHash(t *testing.T) {
+// 	key := "sampleKey"
+// 	Pathname := CASPathTransformFunc(key)
+// 	expectedOriginalKey := "a421c5f473312f80f7984da72bc9c05a7b48cf37"
+// 	ExpectedPathname := "a421c5f473/312f80f798/4da72bc9c0/5a7b48cf37"
+// 	if Pathname.Pathname != ExpectedPathname {
+// 		fmt.Printf("have %s want %s ", Pathname.Pathname, ExpectedPathname)
+// 	}
+// 	if Pathname.Filename != expectedOriginalKey {
+// 		fmt.Printf("have %s want %s ", Pathname.Filename, expectedOriginalKey)
+// 	}
 
-}
+// }
 
 func TestStore(t *testing.T) {
 	opts := StoreOpts{
@@ -37,10 +37,16 @@ func TestStore(t *testing.T) {
 		t.Error(err)
 	}
 
-	b, _ := io.ReadAll(r)
+	b, err := io.ReadAll(r)
+	if err != nil {
+		t.Fatalf("io.ReadAll failed: %v", err)
+	}
 	fmt.Println(string(b))
 	if string(b) != string(data) {
 		t.Errorf("have %s wnat %s", b, data)
 	}
-
+	errr := s.Delete(key)
+	if errr != nil {
+		t.Error(errr)
+	}
 }
